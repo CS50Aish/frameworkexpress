9b1d63d69d8f7e0747b1fb6bce44665a84835c0a
Ubuntu <labuser@ubuntu2204.sbder3d3inmu3lotudpaoql22a.rx.internal.cloudapp.net>
2024-07-13 11:35:06 +0000

    Updated Express Project Files

--- template.js
+++ template.js
@@ -3,7 +3,7 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Hey', message: 'Hello there!'});
+    res.render('index', {title: 'Hey', message: 'Hello there! Lets understand Pug'});
 });
 
 app.listen(3000, () => {
